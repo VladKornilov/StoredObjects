b8ceac984c46016b181d22e3c3259a122f994a86
VladKornilov <36070619+VladKornilov@users.noreply.github.com>
2022-05-25 20:25:47 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kvv_StoredObjects",
-    version="0.0.1",
+    version="0.0.2",
     author="Vladislav Kornilov",
     author_email="v.kornilovv@yandex.ru",
     description="StoredObjects is a helper package with University objects for the Publications Activity Module",
